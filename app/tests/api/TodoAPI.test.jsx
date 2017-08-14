678bee47e00ce56c94be22038971c984bed0704a
Aleksandr Prykhodko <aleksandr.prykhodko@akumyn.com.au>
2017-08-14 18:19:40 +1000

    Add localStorage support

---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,52 @@
+var expect = require('expect');
+
+var TodoAPI = require('TodoAPI');
+
+describe('TodoAPI', () => {
+    beforeEach(()=>{
+        localStorage.removeItem('todos');
+    });
+
+    it('should exist', () => {
+        expect(TodoAPI).toExist();
+    })
+
+    describe('setTodos', () => {
+        it('should set valid todos array', () => {
+            let todos = [{
+                id: 23,
+                text: 'Test all file',
+                completed: false
+            }];
+            TodoAPI.setTodos(todos);
+
+            let actualTodos = JSON.parse(localStorage.getItem('todos'));
+
+            expect(actualTodos).toEqual(todos);
+        });
+        it('should not set invalid todos array', () => {
+            var badTodos = {a: 'b'};
+            TodoAPI.setTodos(badTodos);
+            expect(localStorage.getItem('todos')).toBe(null);
+        });
+    });
+
+    describe('getTodos', () => {
+        it('should return empty array for bad localstorage data', () => {
+            let actualTodos = TodoAPI.getTodos();
+            expect(actualTodos).toEqual([]);
+        });
+
+        it('should return todos if valid array in local storage', () => {
+            let todos = [{
+                id: 23,
+                text: 'Test all file',
+                completed: false
+            }];
+            localStorage.setItem('todos', JSON.stringify(todos));
+            let actualTodos = TodoAPI.getTodos();
+
+            expect(actualTodos).toEqual(todos);
+        });
+    });
+});
