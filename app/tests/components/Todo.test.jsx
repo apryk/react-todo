20174d92212b73357c96e0b499234b10e5d169d5
Aleksandr Prykhodko <aleksandr.prykhodko@akumyn.com.au>
2017-08-14 16:49:05 +1000

    Add toggle support

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -10,4 +10,27 @@ describe('Todo', () => {
     it ('should exist', () => {
         expect(Todo).toExist();
     });
+
+        it('should call onToggle prop with id on click', () => {
+        const todoData = {
+            id: 199,
+            text: 'Write todo.test.jsx test',
+            completed: true
+        };
+        const spy = expect.createSpy();
+        const todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy} />);
+        const $el = $(ReactDOM.findDOMNode(todo));
+        TestUtils.Simulate.click($el[0]);
+
+        expect(spy).toHaveBeenCalledWith(199);
+
+        // todoApp.setState({todos: [todoData]});
+
+        // // check that todos first item has completed cvalue of false
+        // expect(todoApp.state.todos[0].completed).toBe(false);
+        // // call handleToggle with
+        // todoApp.handleToggle(11);
+        // // verify that value changed
+        // expect(todoApp.state.todos[0].completed).toBe(true);
+    });
 })
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -20,4 +20,21 @@ describe('TodoApp', () => {
 
         expect(todoApp.state.todos[0].text).toBe(todoText);
     });
+
+    it('should toggle completed value when handleToggle called', () => {
+        const todoData = {
+            id: 11,
+            text: 'Test features',
+            completed: false
+        };
+        const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+        todoApp.setState({todos: [todoData]});
+
+        // check that todos first item has completed cvalue of false
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        // call handleToggle with
+        todoApp.handleToggle(11);
+        // verify that value changed
+        expect(todoApp.state.todos[0].completed).toBe(true);
+    });
 })
