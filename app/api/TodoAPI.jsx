889a63ec65bf938f1fe61f92933cd520b460f7b7
Aleksandr Prykhodko <aleksandr.prykhodko@akumyn.com.au>
2017-08-16 16:45:09 +1000

    asd

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -27,8 +27,25 @@ module.exports = {
         });
 
         // filter by search text
-        
-        
+        filteredTodos = filteredTodos.filter((todo) => {
+            let text = todo.text.toLowerCase();
+            
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+        // sort todos with nono-completed first
+        filteredTodos.sort((a, b) => {
+            // -1 then a > b, 1 then b > a, 0 therwise
+            if (a.completed === false && b.completed === true) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+            
+        });
+
         return filteredTodos;
     }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -78,5 +78,20 @@ describe('TodoAPI', () => {
             var filteredTodos = TodoAPI.filterTodos(todos, false,'');
             expect(filteredTodos.length).toBe(1);
         });
+
+        it('should sort by completed status', () => {
+            let filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should filter todos by search text', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true,'some');
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should return all todos if search text empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true,'');
+            expect(filteredTodos.length).toBe(3);
+        });
     });
 });
