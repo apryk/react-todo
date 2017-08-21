6f94e5c576f3791d7308b3a5a892ad867db172fb
Aleksandr Prykhodko <aleksandr.prykhodko@akumyn.com.au>
2017-08-21 17:31:44 +1000

    Add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+let uuid = require('uuid');
+let moment = require('moment');
+
 export let searchTextReducer = (state = '', action) => {
     switch (action.type) {
       case 'SET_SEARCH_TEXT':
@@ -7,11 +10,50 @@ export let searchTextReducer = (state = '', action) => {
     };
   };
   
-  export let showCompletedReducer = (state = false, action) => {
-    switch (action.type) {
-      case 'TOGGLE_SHOW_COMPLETED':
-        return !state;
-      default:
-        return state;
-    }
-  };
+export let showCompletedReducer = (state = false, action) => {
+  switch (action.type) {
+    case 'TOGGLE_SHOW_COMPLETED':
+      return !state;
+    default:
+      return state;
+  }
+};
+
+export let todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+    return [
+      ...state,
+      {
+        id: uuid(),
+        text: action.text,
+        completed: false,
+        createdAt: moment().unix(),
+        completedAt: undefined
+      }
+    ];
+    default:
+      return state;
+  }
+};
+
+export let toggleTodoReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          let nextCompleted = !todo.completed;
+
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          };
+
+        }
+      });
+
+    default:
+      return state;
+  }
+}
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,6 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
-
+var moment = require('moment');
 var reducers = require('reducers');
 
 describe('Reducers', () => {
@@ -25,6 +25,42 @@ describe('Reducers', () => {
       var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      let action = {
+        type: 'ADD_TODO',
+        text: 'Walk the cat'
+      };
+      let res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+  });
+
+  describe('toggleTodoReducer', () => {
+    it('should toggle todo', () => {
+      let action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+
+      let todos = [{
+        id: 1,
+        text: "Initial todo",
+        completed: true,
+        createdAt: 123,
+        completedAt: 123
+      }];
+      // res contains new state
+      let res = reducers.toggleTodoReducer(df(todos), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
   });
 });
